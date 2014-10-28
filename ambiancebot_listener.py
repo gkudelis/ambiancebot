8fdb3533fa6dfc7ed66d60167b5f545af27a71f1
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2014-10-28 19:06:15 +0000

    gitignore compiled pys

--- ambiancebot_listener.py
+++ ambiancebot_listener.py
@@ -1,24 +1,31 @@
-import dbm, re
+import settings
+import re, sqlite3
 from twython import TwythonStreamer
 
-db = dbm.open('words', 'c')
+db = sqlite3.connect('words.db')
+c = db.cursor()
 
 class InStream(TwythonStreamer):
     def on_success(self, data):
         if 'text' in data:
+            print "analyzing '" + data['text'] + "'"
             ws = filter(lambda match: match != '',
                 re.findall(r'$| ([a-zA-Z-]+)', data['text'].encode('utf-8')))
             for w in map(lambda word: word.lower(), ws):
+                c.execute("SELECT cnt FROM words WHERE word=?", (w,))
                 try:
-                    current_count = int(db[w])
-                except KeyError:
-                    current_count = 0
-                db[w] = str(current_count + 1)
-            print 'I have these keys: ' + str(db.keys())
+                    cnt, = c.fetchone()
+                    c.execute("UPDATE words SET cnt=? where word=?", (cnt+1, w))
+                except TypeError:
+                    c.execute("INSERT INTO words (word, cnt) VALUES (?, 1)", (w,))
+                db.commit()
 
     def on_error(self, status_code, data):
         print status_code
 
-s = InStream(app_key, app_secret, oauth_token, oauth_token_secret)
+s = InStream(settings.app_key,
+        settings.app_secret,
+        settings.oauth_token, 
+        settings.oauth_token_secret)
 
 s.statuses.filter(track='@ambiancebot')
--- ambiancebot_speaker.py
+++ ambiancebot_speaker.py
@@ -1,24 +1,44 @@
-import dbm, time
+import settings
+
+import time, sqlite3
+from random import randint
+
 from twython import Twython
 from twython.exceptions import TwythonError
 
-db = dbm.open('words', 'r')
 
-t = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
+db = sqlite3.connect('words.db')
+c = db.cursor()
 
+t = Twython(settings.app_key,
+        settings.app_secret,
+        settings.oauth_token,
+        settings.oauth_token_secret)
+
+lastword = ''
 while True:
-    max_count = 0
-    max_word = ''
-    runner_up = ''
-    for w in db.keys():
-        w_count = int(db[w])
-        if w_count > max_count:
-            max_count = w_count
-            runner_up = max_word
-            max_word = w
-    try:
-        t.update_status(status=max_word)
-    except TwythonError:
-        t.update_status(status=runner_up)
+    # get total count of words in db
+    c.execute("SELECT SUM(cnt) FROM words")
+    total_count, = c.fetchone()
+
+    new_word_found = False
+    while not new_word_found:
+        # pick a random number
+        r = randint(1, total_count)
+
+        # go over results and pick one
+        csum = 0
+        for word, cnt in c.execute("SELECT word, cnt FROM words"):
+            csum += cnt
+            if r <= csum:
+                break
+
+        # check if the chosen word is not the same as previous one
+        if word != lastword:
+            new_word_found = True
+
+    print "trying to tweet '" + word + "'"
+    t.update_status(status=word)
+    lastword = word
 
     time.sleep(10*60)
