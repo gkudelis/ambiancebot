5f919faac3cd2e1a4388fd86318061ba787c9588
Giedrius Kudelis <giedrius.kudelis@gmail.com>
2014-10-31 21:17:06 +0000

    Rise of the trigram

---
+++ trigram.py
@@ -0,0 +1,14 @@
+from dynamodb_mapper.model import DynamoDBModel
+
+class Trigram(DynamoDBModel):
+    __table__ = u"digrams"
+    __hash_key__ = u"w12"
+    __range_key__ = u"w3"
+    __schema__ = {
+        u"w12": str,
+        u"w3": str,
+        u"count": int,
+    }
+    __defaults__ = {
+        u"count": 0,
+    }
